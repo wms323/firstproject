09c1ae1b14bad03dd1000b987c0dc65b73d0b6bf
wms <1303263779@qq.com>
2026-07-14 22:39:40 +0800

    写cifar10训练脚本CNN部分

--- cifar_train.py
+++ cifar_train.py
@@ -22,3 +22,29 @@ test_dataset=torchvision.datasets.CIFAR10(root="./data",train=False,download=Tru
 train_dataloader=DataLoader(train_dataset,batch_size=64,shuffle=True)
 test_dataloader=DataLoader(test_dataset,batch_size=64,shuffle=False)
 
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1=nn.Conv2d(3,16,3,padding=1)
+        self.relu1=nn.ReLU()
+        self.pool1nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(2048, 128)
+        self.relu3 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 10)
+
+        def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        x = self.fc2(x)
+        return x
